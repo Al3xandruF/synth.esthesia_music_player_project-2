36f1ae3248cb83061cb2f8a26890ca7f8ad1cb80
Al3xandruF <al3x.florea@gmail.com>
2023-01-05 14:25:15 +0000

    Add semcol between lines 40-50 in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -37,16 +37,16 @@ let isPlaying = false;
 // Play
 function playSong() {
     isPlaying = true;
-    playBtn.classList.replace('fa-play', 'fa-pause')
-    playBtn.setAttribute('title', 'Pause')
+    playBtn.classList.replace('fa-play', 'fa-pause');
+    playBtn.setAttribute('title', 'Pause');
     music.play();
 }
 
 // Pause
 function pauseSong() {
-    isPlaying = false
-    playBtn.classList.replace('fa-pause', 'fa-play')
-    playBtn.setAttribute('title', 'Play')
+    isPlaying = false;
+    playBtn.classList.replace('fa-pause', 'fa-play');
+    playBtn.setAttribute('title', 'Play');
     music.pause();
 }
 
